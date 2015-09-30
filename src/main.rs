5781253dbaa8e36ccd83e7fa9903746c6ffc95ab
Stefan O'Rear <stefanor@cox.net>
2015-09-30 12:45:47 -0700

    A working version

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,33 @@ use std::hash::{Hash,Hasher,SipHasher};
 use std::mem;
 use std::sync::Mutex;
 
-fn vow<T,I,D,S>(sampler: S,
+fn vow<T,I,D,S,C>(sampler: S,
                 distinguished: D,
                 iteration_f: I,
+                check: C,
                 num_threads: u32,
                 loop_timeout: usize,
                 ) -> (T, T)
                 where S : Sync + Fn() -> T,
                       D : Sync + Fn(&T) -> bool,
                       I : Send + Sync + Fn(T) -> T,
+                      C : Sync + Send + Fn(T,T) -> bool,
                       T : Eq + Hash + Clone + Send + Sync {
 
     let mut rv : Option<(T, T)> = None;
+    {
+        let mut rvp = &mut rv;
+        let mut total = 0;
 
-    crossbeam::scope(|scope| {
         let mut done = false;
         let mut known : HashMap<T,(T,usize)> = HashMap::new();
-        let mut rvp = &mut rv;
         let iteration = &iteration_f;
 
         let on_dist = Mutex::new(move |mut start: T, dist: T, mut steps: usize| {
             if done { return true; }
+
+            total += steps;
+            println!("{}", total);
 
             let (mut start2, mut steps2) = match known.entry(dist.clone()) {
                 Entry::Occupied(o) => o.get().clone(),
@@ -61,9 +67,13 @@ fn vow<T,I,D,S>(sampler: S,
                 start = iteration(start);
                 start2 = iteration(start2);
                 if start == start2 {
-                    *rvp = Some((old, old2));
-                    done = true;
-                    return true;
+                    if check(old.clone(), old2.clone()) {
+                        *rvp = Some((old, old2));
+                        done = true;
+                        return true;
+                    } else {
+                        return false;
+                    }
                 }
             }
         });
@@ -80,7 +90,7 @@ fn vow<T,I,D,S>(sampler: S,
                         //if (on_dist.lock().unwrap())(start, state, step_count) {
                             break 'outer;
                         }
-                        break;
+                        continue 'outer;
                     }
                     state = iteration(state);
                 }
@@ -88,29 +98,49 @@ fn vow<T,I,D,S>(sampler: S,
             }
         };
 
-        let mut guards = vec![];
-        for _ in 0 .. num_threads {
-            guards.push(scope.spawn(&worker));
-        }
-    });
+        crossbeam::scope(|scope| {
+            let mut guards = vec![];
+            for _ in 0 .. num_threads {
+                guards.push(scope.spawn(&worker));
+            }
+        });
+    }
 
     rv.unwrap()
 }
 
 fn main() {
-    let (a,b) = vow(
-        || (rand::random::<u64>() & !0xffff),
-        |x| (x & 0xffff0000) == 0,
+    let (mut a,mut b) = vow(
+        || rand::random::<u64>(),
+        |x| (x & 0x1fffff) == 0,
         |x| {
             let mut h = SipHasher::new();
-            x.hash(&mut h);
-            h.finish() & !0xffff
+            10u8.hash(&mut h);
+            ((x & 1) as usize).hash(&mut h);
+            19u8.hash(&mut h);
+            3usize.hash(&mut h);
+            10u8.hash(&mut h);
+            (((x >> 1) & 0x1fffff) as usize).hash(&mut h);
+            5u8.hash(&mut h);
+            1usize.hash(&mut h);
+            10u8.hash(&mut h);
+            (((x >> 22) & 0x1fffff) as usize).hash(&mut h);
+            5u8.hash(&mut h);
+            1usize.hash(&mut h);
+            10u8.hash(&mut h);
+            (((x >> 43) & 0x1fffff) as usize).hash(&mut h);
+            5u8.hash(&mut h);
+            1usize.hash(&mut h);
+            h.finish()
         },
+        |x,y| (x & 1) != (y & 1),
         4,
-        10_000_000,
+        20_000_000,
     );
 
-    println!("{:?}", (a,b));
-    { let mut h = SipHasher::new(); a.hash(&mut h); println!("{}",h.finish()); }
-    { let mut h = SipHasher::new(); b.hash(&mut h); println!("{}",h.finish()); }
+    println!("{:?}",(a,b));
+    if (a & 1) != 0 { mem::swap(&mut a,&mut b); }
+
+    println!("[([u8; {}],[u8; {}],[u8; {}]); 0]", (a >> 1) & 0x1fffff, (a >> 22) & 0x1fffff, (a >> 43) & 0x1fffff);
+    println!("[([u8; {}],[u8; {}],[u8; {}]); 1]", (b >> 1) & 0x1fffff, (b >> 22) & 0x1fffff, (b >> 43) & 0x1fffff);
 }
